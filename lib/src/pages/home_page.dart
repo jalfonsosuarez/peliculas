625edd4a97766df1407eb0cdd1cd11fe30fe5a65
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-23 18:27:31 +0200

    Swiper de tarjetas. Modulo externo que instalar.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,17 +1,55 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
-      SafeArea(
-        child: Text( 'Hola mundoooooooo' ),
+      appBar: AppBar(
+        title: Text('Películas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon( Icons.search ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        )
       )
       
     );
   }
+
+  Widget _swiperTarjetas() {
+
+    return Container(
+      padding: EdgeInsets.only( top: 10.0 ),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+          layout: SwiperLayout.STACK,
+          itemWidth: 200.0,
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+          },
+          itemCount: 3,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
+      ),
+    );
+
+  }
+
 }
