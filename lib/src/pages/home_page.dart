33bc529bce223e9718e0d9e69651f3d1b90f612d
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-23 18:50:37 +0200

    [98] Widget personalizado - CardSwiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
-import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -32,22 +33,8 @@ class HomePage extends StatelessWidget {
 
   Widget _swiperTarjetas() {
 
-    return Container(
-      padding: EdgeInsets.only( top: 10.0 ),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-          layout: SwiperLayout.STACK,
-          itemWidth: 200.0,
-          itemBuilder: (BuildContext context,int index){
-            return new Image.network("http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
-          );
-          },
-          itemCount: 3,
-          pagination: new SwiperPagination(),
-          control: new SwiperControl(),
-      ),
+    return CardSwiper(
+      peliculas: [1,2,3,4,5],
     );
 
   }
