c589e09e67ff9b526da085c904cb910ed58c0ec4
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-23 21:54:27 +0200

    [104] Widget personalizado - Horizintal

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:peliculas/src/providers/pelicula_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 
 
@@ -63,16 +64,21 @@ class HomePage extends StatelessWidget {
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text( 'Populares', 
-                style: Theme.of(context).textTheme.subhead
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text( 'Populares', 
+                  style: Theme.of(context).textTheme.subhead
+            ),
           ),
+          SizedBox(height: 5.0),
           FutureBuilder(
             future:  peliculasProvider.getPopulares(),
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
 
               if (snapshot.hasData ) {
-                return CardSwiper( peliculas: snapshot.data );
+                return MovieHorizontal( peliculas: snapshot.data );
               } else {
                 return Container(
                   height: 400.0,
