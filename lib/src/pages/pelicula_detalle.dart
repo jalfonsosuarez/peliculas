f3618ad1a7bdda3a5dedd94c89415c4b20eb6245
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-24 12:59:39 +0200

    [117] PageView de actores.

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/actores_model.dart';
 import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/pelicula_provider.dart';
 
 class PeliculaDetalle extends StatelessWidget {
 
@@ -20,10 +22,7 @@ class PeliculaDetalle extends StatelessWidget {
                 _descripcion( pelicula ),
                 _descripcion( pelicula ),
                 _descripcion( pelicula ),
-                _descripcion( pelicula ),
-                _descripcion( pelicula ),
-                _descripcion( pelicula ),
-                _descripcion( pelicula ),
+                _crearCasting( pelicula ),
               ]
             ),
           )
@@ -103,6 +102,65 @@ class PeliculaDetalle extends StatelessWidget {
       ),
     );
 
+  }
+
+  Widget _crearCasting( Pelicula pelicula ) {
+
+    final peliProvider = new PeliculasProvider();
+
+    return FutureBuilder(
+      future: peliProvider.getCast(pelicula.id.toString()),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot ) {
+        if ( snapshot.hasData ) {
+          return _crearActoresPageView( snapshot.data );
+        } else {
+          return Center(child: CircularProgressIndicator(),);
+        }
+      },
+    );
+
+
+  }
+
+  Widget _crearActoresPageView( List<Actor> actores ) {
+
+    return SizedBox(
+      height: 200.0,
+      child: PageView.builder(
+        pageSnapping: false,
+        controller: PageController(
+          initialPage: 1,
+          viewportFraction: 0.3,
+        ),
+        itemCount: actores.length,
+        itemBuilder: ( context, i) => _actorTarjeta(actores[i]),
+      ),
+    );
+
+  }
+
+  Widget _actorTarjeta( Actor actor ) {
+
+    return Container(
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular( 20.0 ),
+            child: FadeInImage(
+              image: NetworkImage( actor.getFotoImg() ),
+              placeholder: AssetImage( 'assets/img/no-image.jpg' ),
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text( 
+            actor.name,
+            overflow: TextOverflow.ellipsis,
+          )
+        ],
+      ),
+    );
+  
   }
 
 }
