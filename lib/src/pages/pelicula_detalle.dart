9b60cfde1f1b0e8337aa37d1dc4309be86f35968
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-24 11:35:06 +0200

    [114] Diseño pantalla detalle (2)

--- lib/src/pages/pelicula_detalle.dart
+++ lib/src/pages/pelicula_detalle.dart
@@ -12,6 +12,21 @@ class PeliculaDetalle extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _crearAppbar( pelicula ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10.0,),
+                _posterTitulo( context, pelicula ),
+                _descripcion( pelicula ),
+                _descripcion( pelicula ),
+                _descripcion( pelicula ),
+                _descripcion( pelicula ),
+                _descripcion( pelicula ),
+                _descripcion( pelicula ),
+                _descripcion( pelicula ),
+              ]
+            ),
+          )
         ],
       )
 
@@ -42,4 +57,52 @@ class PeliculaDetalle extends StatelessWidget {
     );
 
   }
+
+  Widget _posterTitulo( BuildContext context, Pelicula pelicula ) {
+
+    return Container(
+      padding: EdgeInsets.symmetric( horizontal: 20.0 ),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage( pelicula.getPosterImg() ),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(width: 20.0,),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(pelicula.title, style: Theme.of(context).textTheme.title, overflow: TextOverflow.ellipsis, ),
+                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subtitle, overflow: TextOverflow.ellipsis,),
+                Row(
+                  children: <Widget>[
+                    Icon( Icons.star_border),
+                    Text( pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subhead )
+                  ],
+                )
+              ],
+            )
+          )
+        ],
+      ),
+    );
+
+  }
+
+  Widget _descripcion( Pelicula pelicula) {
+
+    return Container(
+      padding: EdgeInsets.symmetric( horizontal: 10.0, vertical: 20.0 ),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
+      ),
+    );
+
+  }
+
 }
