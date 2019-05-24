7ff72d96f9c6cbc6f195290b30f11003ed242b04
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-24 13:08:50 +0200

    [118] Hero Animation

--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -47,13 +47,16 @@ class MovieHorizontal extends StatelessWidget {
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20.0),
-            child: FadeInImage(
-              image: NetworkImage( pelicula.getPosterImg()),
-              placeholder: AssetImage( 'assets/img/no-image.jpg'),
-              fit: BoxFit.cover,
-              height: 140.0,
+          Hero(
+            tag: pelicula.id,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: FadeInImage(
+                image: NetworkImage( pelicula.getPosterImg()),
+                placeholder: AssetImage( 'assets/img/no-image.jpg'),
+                fit: BoxFit.cover,
+                height: 140.0,
+              ),
             ),
           ),
           SizedBox( height: 3.0 ),
